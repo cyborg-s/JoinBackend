658f12f118237b3c12eb0d35f47bbbedc252b469
cyborg-s <nysascha1@gmail.com>
2025-05-07 07:11:35 +0200

    UAA Del

--- join_backend/urls.py
+++ join_backend/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('join/', include('join_BE.api.urls')),
-    path('api/auth/', include('user_auth_app.api.urls')),
     path('api/auth/', include('rest_framework.urls'))
 ]
